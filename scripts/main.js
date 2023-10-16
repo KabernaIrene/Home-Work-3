203d27b4fa1970f5cd24fba970da4085c567197a
Kaberna Irene <irinakabernaa@gmail.com>
2023-10-16 18:03:24 +0300

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+const name = window.prompt ("Your name");
+const surname = window.prompt ("Your surname");
+const age = window.prompt ("Your age", 0);
+
+const identify = alert("You are" + " " + name + " " + surname + ", " + age + " " + "years");
+
+const numbers = window.prompt ("Write number");
+
+const number = alert (numbers .split("").join(" "));
+
